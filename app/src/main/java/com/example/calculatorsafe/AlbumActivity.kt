69f1eacf22a2797cefe24eccb28e85fb009adc20
Jonas <jonasschuler14@gmail.com>
2025-01-05 19:07:44 -0600

    Refactor EncryptedImageAdapter into new adapter package

--- app/src/main/java/com/example/calculatorsafe/AlbumActivity.kt
+++ app/src/main/java/com/example/calculatorsafe/AlbumActivity.kt
@@ -4,10 +4,8 @@ import android.Manifest.permission.READ_EXTERNAL_STORAGE
 import android.Manifest.permission.READ_MEDIA_IMAGES
 import android.Manifest.permission.READ_MEDIA_VIDEO
 import android.app.Activity
-import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
@@ -16,10 +14,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.result.ActivityResultLauncher
@@ -27,11 +22,11 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.cardview.widget.CardView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.calculatorsafe.adapters.EncryptedImageAdapter
 import com.example.calculatorsafe.helpers.PermissionHelper
 import com.example.calculatorsafe.helpers.PreferenceHelper.getAlbumId
 import com.example.calculatorsafe.utils.EncryptionUtils
@@ -342,146 +337,4 @@ class AlbumActivity : AppCompatActivity() {
         toolbar.subtitle = "${adapter.itemCount} images"
         // Hide toolbar or action bar
     }
-
-    class EncryptedImageAdapter(
-        private val encryptedFiles: MutableList<File>,
-        private val itemWidth: Int,
-        private val onPhotoClick: (Int) -> Unit,
-        private val decryptFunction: (File) -> Bitmap,
-        private val onEnterSelectionMode: () -> Unit
-        ) : RecyclerView.Adapter<EncryptedImageAdapter.PhotoViewHolder>() {
-
-            enum class Mode {
-                VIEWING,
-                SELECTION
-            }
-
-        var mode = Mode.VIEWING
-            set(value) {
-                field = value
-                notifyDataSetChanged()
-            }
-
-        val selectedItems = mutableSetOf<Int>()
-        var onSelectionChanged: (() -> Unit)? = null
-
-        inner class PhotoViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-            private val cardView: CardView = view.findViewById(R.id.card_view)
-            private val photoImageView: ImageView = view.findViewById(R.id.photo_image_view)
-            private val overlay: View = itemView.findViewById(R.id.overlay) // A semi-transparent View
-
-            fun bind(file: File,position: Int, isSelected: Boolean) {
-                val decryptedBitmap = decryptFunction(file)
-                photoImageView.setImageBitmap(decryptedBitmap)
-                // Show or hide the selection overlay
-                overlay.visibility = if (isSelected) View.VISIBLE else View.GONE
-                itemView.setOnClickListener {
-                    when (mode) {
-                        Mode.VIEWING -> {
-                            onPhotoClick(position)
-                        }
-                        Mode.SELECTION -> {
-                            toggleSelection(position)
-                            notifyItemChanged(position)
-                        }
-                    }
-                }
-                itemView.setOnLongClickListener {
-                    if (mode == Mode.VIEWING) {
-                        toggleSelection(position)
-                        onEnterSelectionMode()
-                    }
-                    true
-                }
-            }
-
-            fun setItemWidth(width: Int) {
-                // Set the width for the CardView
-                val layoutParams = cardView.layoutParams
-                layoutParams.width = width
-                cardView.layoutParams = layoutParams
-
-                layoutParams.height = width // Set the same height as the width to maintain a square shape
-                cardView.layoutParams = layoutParams
-            }
-        }
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoViewHolder {
-            val view = LayoutInflater.from(parent.context).inflate(R.layout.grid_item_layout, parent, false)
-            return PhotoViewHolder(view)
-        }
-
-        override fun onBindViewHolder(holder: PhotoViewHolder, position: Int) {
-            holder.setItemWidth(itemWidth)
-            val encryptedFile = encryptedFiles[position]
-            holder.bind(encryptedFile, position, selectedItems.contains(position))
-        }
-
-        fun toggleSelection(position: Int) {
-            if (selectedItems.contains(position)) {
-                selectedItems.remove(position)
-            } else {
-                selectedItems.add(position)
-            }
-            notifyItemChanged(position)
-            onSelectionChanged?.invoke()
-        }
-
-        fun getSelectedItems(): List<File> {
-            return selectedItems.map { encryptedFiles[it] }
-        }
-
-        fun addFile(file: File) {
-            encryptedFiles.add(file)
-            notifyItemInserted(encryptedFiles.size - 1)
-            FileManager.setFilePaths(encryptedFiles.map { it.absolutePath })
-        }
-
-        // Deletes selected files
-        fun deleteSelectedFiles() {
-            // Sort items from highest to lowest index to avoid index shifting during removal
-            val sortedSelectedItems = selectedItems.sortedDescending()
-            for (position in sortedSelectedItems) {
-                val file = encryptedFiles[position]
-                if (file.exists() && file.delete()) {
-                    // Successfully deleted the file
-                    encryptedFiles.removeAt(position)  // Remove from the list
-                    notifyItemRemoved(position)  // Notify the RecyclerView to update
-                } else {
-                    // Handle failure if necessary, e.g., show a message to the user
-                    Log.e("Delete", "Failed to delete file: ${file.name}")
-                }
-            }
-            selectedItems.clear()  // Clear the selection after deletion
-            FileManager.setFilePaths(encryptedFiles.map { it.absolutePath })
-        }
-
-        fun restoreSelectedFiles(context: Context) {
-            val sortedSelectedItems = selectedItems.sortedDescending()
-            //val selectedFiles = getSelectedItems()
-            for (position in sortedSelectedItems) {
-                val file = encryptedFiles[position]
-                if (file.exists() && EncryptionUtils.restorePhotoToDevice(file, context)) {
-                    // Successfully restored the file
-                    encryptedFiles.removeAt(position)  // Remove from the list
-                    notifyItemRemoved(position)  // Notify the RecyclerView to update
-                } else {
-                    // Handle failure if necessary, e.g., show a message to the user
-                    Log.e("Restore", "Failed to restore file: ${file.name}")
-                }
-            }
-            //EncryptionUtils.restorePhotos(selectedFiles, context)
-            selectedItems.clear()
-            notifyDataSetChanged()
-        }
-
-        fun updateFromFileManager() {
-            encryptedFiles.clear()
-            encryptedFiles.addAll(FileManager.getFilePaths().map { File(it) })
-            notifyDataSetChanged()
-        }
-
-        override fun getItemCount(): Int = encryptedFiles.size
-    }
-
 }
---
+++ app/src/main/java/com/example/calculatorsafe/adapters/EncryptedImageAdapter.kt
@@ -0,0 +1,156 @@
+package com.example.calculatorsafe.adapters
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.calculatorsafe.FileManager
+import com.example.calculatorsafe.R
+import com.example.calculatorsafe.utils.EncryptionUtils
+import java.io.File
+
+class EncryptedImageAdapter(
+    private val encryptedFiles: MutableList<File>,
+    private val itemWidth: Int,
+    private val onPhotoClick: (Int) -> Unit,
+    private val decryptFunction: (File) -> Bitmap,
+    private val onEnterSelectionMode: () -> Unit
+) : RecyclerView.Adapter<EncryptedImageAdapter.PhotoViewHolder>() {
+
+    enum class Mode {
+        VIEWING,
+        SELECTION
+    }
+
+    var mode = Mode.VIEWING
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    val selectedItems = mutableSetOf<Int>()
+    var onSelectionChanged: (() -> Unit)? = null
+
+    inner class PhotoViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val cardView: CardView = view.findViewById(R.id.card_view)
+        private val photoImageView: ImageView = view.findViewById(R.id.photo_image_view)
+        private val overlay: View = itemView.findViewById(R.id.overlay) // A semi-transparent View
+
+        fun bind(file: File, position: Int, isSelected: Boolean) {
+            val decryptedBitmap = decryptFunction(file)
+            photoImageView.setImageBitmap(decryptedBitmap)
+            // Show or hide the selection overlay
+            overlay.visibility = if (isSelected) View.VISIBLE else View.GONE
+            itemView.setOnClickListener {
+                when (mode) {
+                    Mode.VIEWING -> {
+                        onPhotoClick(position)
+                    }
+                    Mode.SELECTION -> {
+                        toggleSelection(position)
+                        notifyItemChanged(position)
+                    }
+                }
+            }
+            itemView.setOnLongClickListener {
+                if (mode == Mode.VIEWING) {
+                    toggleSelection(position)
+                    onEnterSelectionMode()
+                }
+                true
+            }
+        }
+
+        fun setItemWidth(width: Int) {
+            // Set the width for the CardView
+            val layoutParams = cardView.layoutParams
+            layoutParams.width = width
+            cardView.layoutParams = layoutParams
+
+            layoutParams.height = width // Set the same height as the width to maintain a square shape
+            cardView.layoutParams = layoutParams
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.grid_item_layout, parent, false)
+        return PhotoViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: PhotoViewHolder, position: Int) {
+        holder.setItemWidth(itemWidth)
+        val encryptedFile = encryptedFiles[position]
+        holder.bind(encryptedFile, position, selectedItems.contains(position))
+    }
+
+    fun toggleSelection(position: Int) {
+        if (selectedItems.contains(position)) {
+            selectedItems.remove(position)
+        } else {
+            selectedItems.add(position)
+        }
+        notifyItemChanged(position)
+        onSelectionChanged?.invoke()
+    }
+
+    fun getSelectedItems(): List<File> {
+        return selectedItems.map { encryptedFiles[it] }
+    }
+
+    fun addFile(file: File) {
+        encryptedFiles.add(file)
+        notifyItemInserted(encryptedFiles.size - 1)
+        FileManager.setFilePaths(encryptedFiles.map { it.absolutePath })
+    }
+
+    // Deletes selected files
+    fun deleteSelectedFiles() {
+        // Sort items from highest to lowest index to avoid index shifting during removal
+        val sortedSelectedItems = selectedItems.sortedDescending()
+        for (position in sortedSelectedItems) {
+            val file = encryptedFiles[position]
+            if (file.exists() && file.delete()) {
+                // Successfully deleted the file
+                encryptedFiles.removeAt(position)  // Remove from the list
+                notifyItemRemoved(position)  // Notify the RecyclerView to update
+            } else {
+                // Handle failure if necessary, e.g., show a message to the user
+                Log.e("Delete", "Failed to delete file: ${file.name}")
+            }
+        }
+        selectedItems.clear()  // Clear the selection after deletion
+        FileManager.setFilePaths(encryptedFiles.map { it.absolutePath })
+    }
+
+    fun restoreSelectedFiles(context: Context) {
+        val sortedSelectedItems = selectedItems.sortedDescending()
+        //val selectedFiles = getSelectedItems()
+        for (position in sortedSelectedItems) {
+            val file = encryptedFiles[position]
+            if (file.exists() && EncryptionUtils.restorePhotoToDevice(file, context)) {
+                // Successfully restored the file
+                encryptedFiles.removeAt(position)  // Remove from the list
+                notifyItemRemoved(position)  // Notify the RecyclerView to update
+            } else {
+                // Handle failure if necessary, e.g., show a message to the user
+                Log.e("Restore", "Failed to restore file: ${file.name}")
+            }
+        }
+        //EncryptionUtils.restorePhotos(selectedFiles, context)
+        selectedItems.clear()
+        notifyDataSetChanged()
+    }
+
+    fun updateFromFileManager() {
+        encryptedFiles.clear()
+        encryptedFiles.addAll(FileManager.getFilePaths().map { File(it) })
+        notifyDataSetChanged()
+    }
+
+    override fun getItemCount(): Int = encryptedFiles.size
+}
